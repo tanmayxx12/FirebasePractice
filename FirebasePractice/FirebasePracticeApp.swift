9ff6459d3ad26eb9a21d1e0eceba23a076381e1a
tanmayxx12 <troy12rollsza@gmail.com>
2025-01-29 14:00:38 +0530

    Initial Commit

---
+++ FirebasePractice/FirebasePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePracticeApp.swift
+//  FirebasePractice
+//
+//  Created by Tanmay . on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
