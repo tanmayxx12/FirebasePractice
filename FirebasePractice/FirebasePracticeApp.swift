1392f8f0e0544816ad535e21fdcd7333ec3baa9a
tanmayxx12 <troy12rollsza@gmail.com>
2025-01-30 15:44:56 +0530

    [Set up] Set up Firebase and Google Sign In in the apps entry point

--- FirebasePractice/FirebasePracticeApp.swift
+++ FirebasePractice/FirebasePracticeApp.swift
@@ -6,18 +6,34 @@
 //
 
 import FirebaseCore
+import GoogleSignIn
+import GoogleSignInSwift
 import SwiftUI
 
 @main
 struct FirebasePracticeApp: App {
     
-    init() {
-        FirebaseApp.configure()
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil)  -> Bool {
+        // Initialize Firebase:
+        FirebaseApp.configure()
+        
+        // Configure Google Sign In:
+        GIDSignIn.sharedInstance.configuration = GIDConfiguration(clientID: FirebaseApp.app()?.options.clientID ?? "")
+        return true
+    }
+}
+
+// Handle Google Sign-In URL redirects:
+func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+    return GIDSignIn.sharedInstance.handle(url)
 }
